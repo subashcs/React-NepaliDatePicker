381a685cd0f26502a16b1889d0318d513eadad49
Subash Chandra Sapkota <scs@logpoint.com>
2023-02-02 20:22:12 +0545

    chore: update package name, packages deps

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-" React Nepali Date Picker" 
+## React Nepali Date Picker
+Nepali date picker UI input field component for React 
+
+## Usage
+
+
+```
+import { NepaliC } from "nepalidatepicker-react";
+
+```
+
+```
+   <NepaliC
+        value={date}
+        readOnly={false}
+        default={"१२-१२-१९९९"}
+        format={"MM-DD-YYYY"}
+        onChangeHandler={handleChange}
+      />
+```
--- src/module/index.js
+++ src/module/index.js
@@ -1,32 +1,32 @@
-import React ,{useState}from 'react';
-import { NepaliCalender,NepaliC } from '../lib';
+import React, { useState } from "react";
+import { NepaliCalender, NepaliC } from "../lib";
 
-const App =()=>{
- const [date, setDate] = useState("");
- const handleChange=(e)=>{
-   setDate(e.target.value);
-   console.log("latest date",date);
- }
-//  <NepaliCalender
-//                         value={date}
-//                         readOnly={false}
-//                         default = {"१२-१२-१९९९"}
-//                         format={"MM-DD-YYYY"}
-//                         onChangeHandler={handleChange}
-//                     />
-    return(
-      <div>
-
-                    <NepaliC
-                    value={date}
-                    readOnly={false}
-                    default = {"१२-१२-१९९९"}
-                    format={"MM-DD-YYYY"}
-                    onChangeHandler={handleChange}
-                />
-      </div>
-
-    );
+const App = () => {
+  const [date, setDate] = useState("");
+  const handleChange = (e) => {
+    setDate(e.target.value);
+    console.log("latest date", date);
+  };
+  //  <NepaliCalender
+  //    value={date}
+  //    readOnly={false}
+  //    default={"१२-१२-१९९९"}
+  //    format={"MM-DD-YYYY"}
+  //    onChangeHandler={handleChange}
+  //  />;
+  return (
+    <div>
+      <NepaliC
+        value={date}
+        readOnly={false}
+        default={"१२-१२-१९९९"}
+        format={"MM-DD-YYYY"}
+        onChangeHandler={handleChange}
+      />
+      <h2>Selected Date</h2>
+      <h2>{date}</h2>
+    </div>
+  );
 };
 
 export default App;
